d62a8bb51d3489465dc30b458be1eddac550150f
Nesser Andrade <nesser24601@live.com>
2021-09-24 20:05:08 -0300

    exx006

--- ex006/script.js
+++ ex006/script.js
@@ -1,13 +1,29 @@
+const estadosBrasileiros = Object.freeze({
+    AC: 'Acre', AL: 'Alagoas', PR: 'Paraná'
+})
+
 class Endereco {
     constructor(logradouro, numero, cidade, estado, pais, cep) {
         this.logradouro = logradouro
         this.numero = numero
         this.cidade = cidade
-        this.estado = estado
+        this.estado = this.checaEstado(estado)
         this.pais = pais
         this.cep = cep
+    }
+    checaEstado(estado) {
+        for (const enumEstado of Object.values(estadosBrasileiros)) {
+            if (enumEstado == estado) {
+                return estado
+            }
+        }
+        console.log("Estado não encontrado")
+        return false
     }
 }
 
 let cliente1 = new Endereco("Rua Brasil", 922, "Londrina", "Paraná", "Brasil", "86010200")
+let cliente2 = new Endereco("Rua Brasil", 922, "Londrina", "São Paulo", "Brasil", "86010200")
+
 console.log(cliente1)
+console.log(cliente2)
