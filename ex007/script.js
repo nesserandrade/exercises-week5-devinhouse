80a72392ad91be29f82b1b88702bd1c122c25b40
Nesser Andrade <nesser24601@live.com>
2021-09-25 10:54:00 -0300

    alterações no 7

--- ex007/script.js
+++ ex007/script.js
@@ -11,8 +11,8 @@ class Cliente {
     Soma = 0;
     if (cpf == "00000000000") return "";
 
-    for (let i=1; i<=9; i++) {Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
-    Resto = (Soma * 10) % 11}
+    for (let i=1; i<=9; i++) {Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);}
+    Resto = (Soma * 10) % 11
 
     if ((Resto == 10) || (Resto == 11))  {Resto = 0}
     if (Resto != parseInt(cpf.substring(9, 10))) {
@@ -34,6 +34,6 @@ class Cliente {
     }
 }
 
-let cliente1 = new Cliente('Nesser', '07189021905', 'Rua Brasil, 922', 43999999999)
+let cliente1 = new Cliente('Nesser', '11111111111', 'Rua Brasil, 922', 43999999999)
 
 console.log(cliente1)
